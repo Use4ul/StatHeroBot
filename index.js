4fa5a47caf4badc378ccb33f561afc7b5e14a496
Use4ul <i.am.use4ul@gmail.com>
2025-04-06 00:22:10 +0300

    created develop

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json()); // Для парсинга JSON
+app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Для данных из форм
 app.use(express.static('public')); // Раздача статики (css, js, изображения)
 
